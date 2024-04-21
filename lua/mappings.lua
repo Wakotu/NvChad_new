e94453474937e4b683249241e1d5157d1bc9bae1
Wakotu <442444286@qq.com>
2024-04-21 19:50:52 +0800

    update basic mapping

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -4,7 +4,18 @@ require "nvchad.mappings"
 
 local map = vim.keymap.set
 
-map("n", ";", ":", { desc = "CMD enter command mode" })
-map("i", "jk", "<ESC>")
+-- map("n", ";", ":", { desc = "CMD enter command mode" })
+map("n", "<leader>ww", ":wqa<cr>", {desc = "save and quit all"})
+
+-- buffer, window, and tab
+map("n", "H", ":bp<cr>", {desc = "Go to previous buffer"})
+map("n", "H", ":bn<cr>", {desc = "Go to next buffer"})
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+map("i", "kk", "<ESC>", { desc = "exit insert mode"})
+
+
+local nomap = vim.keymap.del
+
+nomap("n", "<S-Tab>")
+nomap("n", "<Tab>")
